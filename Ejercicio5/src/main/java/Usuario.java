511b7f39a6b277c7b8478beeef1db4fb0064d282
Salvador Sobrino <salvador.sobrino.solorzano.1p@gmail.com>
2022-03-11 12:41:42 +0100

    subida del ejercicio

---
+++ Ejercicio5/src/main/java/Usuario.java
@@ -0,0 +1,33 @@
+
+public class Usuario {
+	private String usuario;
+	private String contrasena;
+	
+	public Usuario(String usuario, String contrasena) {
+		super();
+		this.usuario = usuario;
+		this.contrasena = contrasena;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [usuario=" + usuario + ", contrasena=" + contrasena + "]";
+	}
+	
+}
